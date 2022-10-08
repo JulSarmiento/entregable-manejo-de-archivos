8c10e7e7f85c2c574fdc2266b9b7a6ef5c694b29
JulSarmiento <jasa1999@hotmail.com>
2022-10-08 08:48:48 -0500

    primera funcion del contrustor container funcionando con la base de datos: readFiles

--- classes/container.class.js
+++ classes/container.class.js
@@ -1,5 +1,57 @@
+const fs = require("fs");
+const readFiles = require("../utilities/readfile");
+const saveFiles = require("../utilities/saveFile");
+
 class Container {
   constructor(filename) {
-    this._filename = ``
+    this._filename = `db/${filename}.txt`;
   }
+
+  /**
+   * Retorna ID
+   * @param {object} product 
+   */
+  save(product){
+
+  };
+
+  /**
+   * returb product object
+   * @param {number} productId 
+   */
+  getbyId(productId){
+
+  };
+
+  /**
+   * 
+   * @returns array
+   */
+  async getAll(){
+    try{
+      return await readFiles(this._filename);
+    }
+    catch (err) {
+      console.error(err);
+    }
+    
+  };
+
+  /**
+   * no return
+   * @param {number} productId 
+   */
+  deleteById(productId){
+
+  };
+
+  /**
+   * No return
+   */
+  deleteAll(){
+
+  };
 }
+
+
+module.exports = Container;
---
+++ main.js
@@ -0,0 +1,14 @@
+const Container = require("./classes/container.class");
+
+const db = "products";
+
+const product = new Container(db);
+
+const productExample = {
+    title: 'Collar Elegant',
+    price: 152000,
+    thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mazu-store.appspot.com/o/products%2Fcollar-elegant%2Fcollar-elegant-ambar-back.jpg?alt=media&token=8985f23e-b5f3-4a34-a388-cef4ed8abb4d'
+};
+
+product.getAll()
+    .then(console.log);
